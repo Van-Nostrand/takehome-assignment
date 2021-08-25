6849e39768c310c7290147824c25852136132843
Van-Nostrand <dmdoull43@gmail.com>
2021-08-25 16:59:44 -0700

    new bundle for github pages

--- index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./src/App";
-import "./src/scss/main.scss";
-
-
-ReactDOM.render(<App />, document.getElementById("root"));
